2bfef3ce385527a3cf2a5b603581f9902eb2ff76
Vaibhav91one <vtking03@gmail.com>
2023-12-11 16:20:12 +0530

    Project Uploading

---
+++ Frontend/src/components/ChallengeCard.vue
@@ -0,0 +1,110 @@
+<template>
+    <v-card class="mx-auto" max-width="500" style="border-radius: 5px;">
+        <v-img :src=imageURL height="300px" cover></v-img>
+
+        <v-card-title>
+            {{ title }}
+        </v-card-title>
+
+        <v-card-subtitle>
+            <span :style="{ color: Level === 'Easy' ? 'green' : (Level === 'Moderate' ? 'yellow' : 'red') }"> {{ Level }}
+            </span>
+
+        </v-card-subtitle>
+
+        <v-card-actions>
+            <v-btn variant="text" style="background-color: black; color: white; padding: 10px; margin: 10px;">
+                <v-row justify="center">
+                    <v-dialog v-model="dialog" width="540">
+                        <template v-slot:activator="{ props }">
+                            <v-btn color="white" v-bind="props">
+                                Try
+                            </v-btn>
+                        </template>
+                        <v-card>
+                            <v-card-title style="justify-content: center; display: flex; margin-top: 20px; padding: 10px;">
+                                <span class="text-h5">{{ title }}</span>
+                            </v-card-title>
+                            <v-card-text>
+                                <v-container>
+                                    <v-row>
+                                        <v-card-text style="margin: 10px; padding: 10px;">
+                                            {{ description }}
+                                        </v-card-text>
+                                        <v-col cols="12">
+                                            <v-text-field label="{Flag}"
+                                                hint="Make sure you have gotten the right one!"></v-text-field>
+                                        </v-col>
+                                    </v-row>
+                                </v-container>
+                            </v-card-text>
+                            <v-card-actions style="margin: 10px; padding: 10px;">
+                                <v-btn color="primary">
+                                    Hint
+
+                                    <v-dialog v-model="dialogHint" activator="parent" width="auto">
+                                        <v-card>
+                                            <v-card-text>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                                tempor incididunt ut labore et dolore magna aliqua.
+                                            </v-card-text>
+                                        </v-card>
+                                    </v-dialog>
+                                </v-btn>
+                                <v-spacer></v-spacer>
+                                <v-btn color="white" variant="text" @click="dialog = false" style="background-color: black;">
+                                    Submit
+                                </v-btn>
+                                <v-spacer></v-spacer>
+                                <v-btn color="red">
+                                    Write Up
+
+                                    <v-dialog v-model="dialogWriteUp" activator="parent" width="auto">
+                                        <v-card>
+                                            <v-card-text>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                                tempor incididunt ut labore et dolore magna aliqua.
+                                            </v-card-text>
+                                            <!-- <v-card-actions>
+                                                <v-btn color="primary" block @click="dialogWriteUp = false">Close Dialog</v-btn>
+                                            </v-card-actions> -->
+                                        </v-card>
+                                    </v-dialog>
+                                </v-btn>
+                               
+                                
+                            </v-card-actions>
+                        </v-card>
+                    </v-dialog>
+                </v-row>
+            </v-btn>
+
+            <v-spacer></v-spacer>
+
+            <v-btn :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show = !show"></v-btn>
+        </v-card-actions>
+
+        <v-expand-transition>
+            <div v-show="show">
+                <v-divider></v-divider>
+
+                <v-card-text>
+                    {{ description }}
+                </v-card-text>
+            </div>
+        </v-expand-transition>
+    </v-card>
+</template>
+<script>
+export default {
+    props: ['title', 'description', 'Level', 'imageURL'],
+    data: () => ({
+        show: false,
+        dialog: false,
+        dialogHint: false,
+        dialogWriteUp: false
+    }),
+}
+
+
+</script>
